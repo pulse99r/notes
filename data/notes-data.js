d7f45cdb8605c9549441196c021724df85338b12
Raymond <raymondjohnson@pursuit.org>
2023-11-11 19:47:33 -0500

    initiated refactoring of the data shown page

---
+++ data/notes-data.js
@@ -0,0 +1,31 @@
+const data = [
+  {topic: `How to Phrase it Without Saying "You're Wrong!"`,
+  intro: `Expressing alternative ideas, processes, and methods assertively without making others feel like they're wrong requires careful communication. Here are some tips:`,
+  1: `Use "I" Statements**: Instead of saying "you should do this", say "I suggest" or "I believe". This shifts the focus from what the other person is doing wrong to what you believe could be done differently.`,
+  2: `Acknowledge Other Views**: Before presenting your alternative, acknowledge the current method or idea. You might say something like, "I understand that currently we are doing X, and it has been effective in these ways...".`,
+  3: `Present Alternatives as Options**: Introduce your alternative as another option to consider, rather than the only correct way. For example, you could say, "One option we might consider is...".`,
+  4: `Use Positive Language**: Frame your alternative in a positive light. For example, instead of saying "This process is too slow", you could say "We might speed up the process by...".`,
+  5: `Explain the Benefits**: Clearly outline the benefits of your alternative idea, process, or method. If possible, provide evidence or examples to support your points.`,
+  6: `Be Open to Discussion**: Make it clear that you're open to discussion and feedback about your alternative. Encourage others to ask questions and share their thoughts.`,
+  7: `Avoid Absolute Terms**: Avoid using words like "always", "never", "best", or "worst". These words can make your alternative seem like the only correct option, which can be off-putting.`,
+  8: `Remember, the goal is to communicate your ideas in a way that is respectful of others' opinions and open to collaboration.`
+  },
+  {topic: 'Agreement vs Alignment',
+  1: `Strong relationships don't need agreement. They need alignment.`,
+  2: `Agreement is having identical opinions. Alignment is having shared values.`,
+  3: `Agreement is taking the same path. Alignment is heading in the same direction`,
+  4: `Closeness is a matter of commitment, not consensus.`
+  },
+  {topic: 'HTML Notes'},
+  {topic: 'CSS Notes'},
+  {topic: 'JavaScript Notes'},
+  {topic: 'Express Coding for Backend Servers'},
+  {topic: 'SQL Cheatsheet'},
+  {topic: 'React Cheatshet'},
+  {topic: 'PERN Concepts'},
+  {topic: 'Data Structures'},
+  {topic: 'Web Development Roles'},
+  {topic: 'Example of an Image Carousel'},
+  {topic: 'How to Build An Image Carousel'},
+  {topic: 'Test of adding multiple classes to an HTML Node'}
+]
